fb99a399a9a04b7161d6d40f1aa6143589c97271
kattak <katrina.devaney@gmail.com>
2017-01-09 07:58:26 -0800

    Add notes from SO to current_user for easy reference

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,34 @@ module SessionsHelper
   # # Log in from the console to create a session
   # app.post '/login', {"authenticity_token"=>"5aMTqep2Tp35PP7G+v50AjNdiI2PKHmWeF+Lv3ZflQk=",  "user[login]"=>'chloe', 'user[password]'=>'test'}
 
+#Notes from SO:
+#   wow amazing, how to post/get/session stuff in console:
+# http://stackoverflow.com/a/23899701
+#
+# # Start Rails console
+# rails console
+# # Get the login form
+# app.get '/login'
+# # View the session
+# app.session.to_hash
+# # Copy the CSRF token "_csrf_token" and place it in the login request.
+# # Log in from the console to create a session
+# app.post '/login', {"authenticity_token"=>"5aMTqep2Tp35PP7G+v50AjNdiI2PKHmWeF+Lv3ZflQk=",  "user[login]"=>'chloe', 'user[password]'=>'test'}
+# # View the session to verify CSRF token is the same
+# app.session.to_hash
+# # Copy the CSRF token "_csrf_token" and place it in the request. It's best to edit this in Notepad++
+# app.post '/refinery/blog/posts', {"authenticity_token"=>"gT7G17RNFaWUDLC6PJGapwHk/OEyYfI1V8yrlg0lHpM=", "switch_locale"=>"en", "post"=>{"title"=>"Test", "homepage"=>"0", "featured"=>"0", "magazine"=>"0", "refinery_category_ids"=>["1282"], "body"=>"Tests do a body good.", "custom_teaser"=>"", "draft"=>"0", "tag_list"=>"", "published_at(1i)"=>"2014", "published_at(2i)"=>"5", "published_at(3i)"=>"27", "published_at(4i)"=>"21", "published_at(5i)"=>"20", "custom_url"=>"", "source_url_title"=>"", "source_url"=>"", "user_id"=>"56", "browser_title"=>"", "meta_description"=>""}, "continue_editing"=>"false", "locale"=>:en}
+# Might be useful:
+# app.cookies.to_hash
+# app.flash.to_hash
+# app.response # long, raw, HTML
+#
+# add helper methods to your rails console
+# http://opensoul.org/2012/11/08/add-helper-methods-to-your-rails-console/
+#
+# ohhhh amazing
+
+
 #hi function to test access to helper methods in rails console
   def hi
     return "hi"
